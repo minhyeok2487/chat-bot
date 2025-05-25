9d1f6b171c83c1e746b61110f3926c065d82a9b7
Minhyeok1995 <repeater2487@naver.com>
2025-05-25 17:14:59 +0900

    프로젝트 구조 설계

---
+++ src/main/kotlin/sionic/minhyeok/MinhyeokApplication.kt
@@ -0,0 +1,11 @@
+package sionic.minhyeok
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinhyeokApplication
+
+fun main(args: Array<String>) {
+	runApplication<MinhyeokApplication>(*args)
+}
---
+++ src/test/kotlin/sionic/minhyeok/MinhyeokApplicationTests.kt
@@ -0,0 +1,13 @@
+package sionic.minhyeok
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinhyeokApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
